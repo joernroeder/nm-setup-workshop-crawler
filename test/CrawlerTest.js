5e0a36b4b1f5ff33df9270bb077980e708b8323a
Jörn Röder <kontakt@joernroeder.de>
2014-12-02 13:22:52 +0100

    testing the test suite

---
+++ test/CrawlerTest.js
@@ -0,0 +1,8 @@
+var should = require('should');
+
+describe('Crawler test suite', function () {
+
+	it ('should correctly create a new crawler', function () {
+		(5).should.be.exactly(5).and.be.a.Number;
+	});
+});
