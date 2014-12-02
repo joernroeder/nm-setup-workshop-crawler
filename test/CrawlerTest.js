9873be0743aa8dfc3946086c6db3f30630511dea
Jörn Röder <kontakt@joernroeder.de>
2014-12-02 14:35:17 +0100

    added first cleanupUrl test

--- test/CrawlerTest.js
+++ test/CrawlerTest.js
@@ -10,4 +10,20 @@ describe('Crawler test suite', function () {
 		crawler.should.be.an.instanceOf(Crawler);
 		crawler.url.should.equal('http://spiegel.de');
 	});
+
+	describe ('#cleanupUrl', function () {
+
+		it ('should correctly return an error if the url is not an absolute path', function (done) {
+			var crawler = new Crawler('', {});
+
+			crawler.cleanupUrl('foo/index.html', function (err, url) {
+				err.should.be.an.instanceOf(Error);
+				err.message.should.equal('the given url is not an absolute url!');
+				(url === null).should.be.true;
+
+				done();
+			});
+		});
+
+	});
 });
